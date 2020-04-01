8f864349b61468e2748bad51b363bce5c15725fa
3JUT7 <62133932+3JUT7@users.noreply.github.com>
2020-04-01 10:18:14 +0200

    Creating a class for the Token

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -5,5 +5,7 @@ public class Config{
 
     public static String PREFIX = "#";
     public static String HELPMASSAGE = "Hilfe kriegst du bei <@418861325435797514>";
+    public static String test = "";
+
 
 }
